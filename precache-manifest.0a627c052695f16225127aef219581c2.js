d23cdaf337240001ff26c7cc6026703606046591
Gokulakrishnan <gokulakrishnanmay29@gmail.com>
2020-10-14 02:50:41 +0530

    Updates

---
+++ precache-manifest.0a627c052695f16225127aef219581c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cb1bf1acfa99cf17ab29d4f49053a1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "40fed961e3a8b0d8dd43",
+    "url": "/monster-rolodex/static/css/main.774cb17c.chunk.css"
+  },
+  {
+    "revision": "4b4d008964b1136230e0",
+    "url": "/monster-rolodex/static/js/2.e373ee96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e373ee96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fed961e3a8b0d8dd43",
+    "url": "/monster-rolodex/static/js/main.80487a97.chunk.js"
+  },
+  {
+    "revision": "eb6d617845ccf827ff5f",
+    "url": "/monster-rolodex/static/js/runtime-main.632c43e9.js"
+  }
+]);
